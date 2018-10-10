e3420e25c915807e164a9aaec73cfb9fcc0b3f9c
erm <jermff@gmail.com>
2018-10-11 00:00:37 +1100

    Update test app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from loamy.serializers import Serializer
-from loamy.fields import StrField, IntField
+from loamy.fields import String, Integer, Float, Number
 
 
 class MySerializer(Serializer):
-    mystr = StrField()
-    myint = IntField()
+    mystr = String(min=1, max=10)
+    mynullstr = String(null=True)
+    mybytes = String()
+    myint = Integer(min=1)
+    myfloat = Float(min=0.001)
+    mynumber = Number()
 
 
-s = MySerializer()
+request_data = {
+    "mystr": "mystr",
+    "myint": 1,
+    "mybytes": b"mybytes",
+    "myfloat": 0.01,
+    "mynumber": 1.0,
+}
 
-s(mystr="test", myint=1)
+s = MySerializer(**request_data)
 s.validate()
