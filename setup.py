13b1fd6168b96c143ddb70e5c0bef85548616b43
erm <jermff@gmail.com>
2018-10-11 01:12:44 +1100

    More details in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,32 @@ from setuptools import find_packages, setup
 
 from loamy.__version__ import __version__
 
+
+def get_long_description():
+    """Return the README."""
+    return open("README.md", "r", encoding="utf8").read()
+
+
 setup(
     name="loamy",
     version=__version__,
     packages=find_packages(),
-    license="MIT License",
+    license="MIT",
     url="https://github.com/erm/loamy",
     description="Something something something REST APIs...",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     author="Jordan Eremieff",
     author_email="jordan@eremieff.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
